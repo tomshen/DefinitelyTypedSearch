b7327816f9616e6b639eaab47c70b18d63d318bf
Tom Shen <tshen@palantir.com>
2015-12-16 15:32:55 -0500

    Add search

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
+import lunr from "lunr";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { getAllPackages } from "./github";
 import Table from "./table";
-import { getAllPackages } from "./typings";
+
+const searchIndex = lunr(function () {
+    this.field("packageName");
+    this.ref("id");
+});
 
 getAllPackages()
     .then(packages => {
-        ReactDOM.render(<Table packages={packages}/>, document.getElementById("container"));
+        packages.forEach((packageName, index) => searchIndex.add({
+            id: index,
+            packageName: packageName
+        }));
+        ReactDOM.render(
+            <Table packages={packages} searchIndex={searchIndex}/>,
+            document.getElementById("container")
+        );
     });
--- src/table.jsx
+++ src/table.jsx
@@ -1,17 +1,35 @@
 import React from "react";
 
 export default class Table extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ""
+        };
+    }
+
+    handleQueryChange(event) {
+        this.setState({
+            query: event.target.value
+        });
+    }
     render() {
-        return <ul className="search-table">
-        {
-            this.props.packages.map((packageName, index) => {
-                return <li key={index}>
-                    <a href={`https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/${packageName}`}>
-                        {packageName}
-                    </a>
-                </li>;
-            })
-        }
-        </ul>
+        const packages = this.state.query.length === 0 ? this.props.packages
+            : this.props.searchIndex.search(this.state.query).map(result => this.props.packages[result.ref]);
+
+        return <div className="search-table">
+            <input type="text" value={this.state.query} onChange={(event) => this.handleQueryChange(event)} />
+            <ul>
+            {
+                packages.map((packageName, index) => {
+                    return <li key={index}>
+                        <a href={`https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/${packageName}`}>
+                            {packageName}
+                        </a>
+                    </li>;
+                })
+            }
+            </ul>
+        </div>;
     }
 }
